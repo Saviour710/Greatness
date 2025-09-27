148ca5fc3a360ad93eb966ad5e80a2d888fc9b7f
Saviour710 <ceasarsaviour@gmail.com>
2025-09-27 10:01:08 +0300

    Add main.dart with basic Flutter main function

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, Flutter!'),
+        ),
+        body: Center(
+          child: Text('Welcome to Flutter!'),
+        ),
+      ),
+    );
+  }
+}
