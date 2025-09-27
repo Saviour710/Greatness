5d75ddbada437d0a92ec6422fe01bdaffb438cac
Saviour710 <ceasarsaviour@gmail.com>
2025-09-27 19:47:56 +0300

    Create signup_screen.dart for Signup Screen

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Signup Screen')), 
+      body: Center(child: Text('Signup Screen')), 
+    );
+  }
+}
