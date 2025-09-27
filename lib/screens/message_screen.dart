5775ee64d15b0b6fbe305ab592c8fc7e0288205b
Saviour710 <ceasarsaviour@gmail.com>
2025-09-27 19:49:03 +0300

    Create message_screen.dart

---
+++ lib/screens/message_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MessageScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Message Screen')), 
+      body: Center(child: Text('Message Screen')), 
+    );
+  }
+}
